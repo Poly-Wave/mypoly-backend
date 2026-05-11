3fbef39fe15db75a83c854b051d8626de12362be
munis <aaaa6658@naver.com>
2026-05-11 22:24:05 +0900

    fix(bill-service) : 의안 검색 requiredMode 추가

--- bill-service/src/main/java/com/polywave/billservice/api/dto/SearchAgendaResponse.java
+++ bill-service/src/main/java/com/polywave/billservice/api/dto/SearchAgendaResponse.java
@@ -2,17 +2,31 @@ package com.polywave.billservice.api.dto;
 
 import com.polywave.billservice.application.agenda.query.result.SearchAgendaResult;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import java.time.LocalDate;
 
 @Schema(description = "의안 검색 결과 응답")
 public record SearchAgendaResponse(
-        @Schema(description = "의안 ID", example = "1", requiredMode = Schema.RequiredMode.REQUIRED) Long billId,
-        @Schema(description = "주제 코드", example = "DIGITAL") String categoryCode,
-        @Schema(description = "주제 이름", example = "디지털") String categoryName,
-        @Schema(description = "제목", example = "○○법 일부개정법률안", requiredMode = Schema.RequiredMode.REQUIRED) String title,
-        @Schema(description = "등록일자", example = "2026-04-16", requiredMode = Schema.RequiredMode.REQUIRED) LocalDate registeredDate,
-        @Schema(description = "조회수", example = "0", requiredMode = Schema.RequiredMode.REQUIRED) long viewCount,
-        @Schema(description = "투표수", example = "123", requiredMode = Schema.RequiredMode.REQUIRED) long voteCount) {
+        @Schema(description = "의안 ID", example = "1", requiredMode = RequiredMode.REQUIRED)
+        Long billId,
+
+        @Schema(description = "주제 코드", example = "DIGITAL", requiredMode = RequiredMode.REQUIRED)
+        String categoryCode,
+
+        @Schema(description = "주제 이름", example = "디지털", requiredMode = RequiredMode.REQUIRED)
+        String categoryName,
+
+        @Schema(description = "제목", example = "○○법 일부개정법률안", requiredMode = RequiredMode.REQUIRED)
+        String title,
+
+        @Schema(description = "등록일자", example = "2026-04-16", requiredMode = RequiredMode.REQUIRED)
+        LocalDate registeredDate,
+
+        @Schema(description = "조회수", example = "0", requiredMode = RequiredMode.REQUIRED)
+        long viewCount,
+
+        @Schema(description = "투표수", example = "123", requiredMode = RequiredMode.REQUIRED)
+        long voteCount) {
 
     public static SearchAgendaResponse from(SearchAgendaResult result) {
         return new SearchAgendaResponse(
