5120df8dee115f8ed7c3f93e4438dcd7104be8d9
seongyonglim <seongyonglim@naver.com>
2026-07-20 20:35:35 +0900

    fix(bill-batch): 표결 동기화 대상 선정 순서 및 API 에러 처리 버그 수정

--- bill-batch/app/config.py
+++ bill-batch/app/config.py
@@ -164,7 +164,7 @@ class Settings:
 
             bill_batch_vote_sync_missing_only=_parse_bool(_get_env("BILL_BATCH_VOTE_SYNC_MISSING_ONLY", "true"), default=True),
             bill_batch_vote_sync_lookback_days=int(_get_env("BILL_BATCH_VOTE_SYNC_LOOKBACK_DAYS", "365")),
-            bill_batch_vote_sync_max_target_bills=int(_get_env("BILL_BATCH_VOTE_SYNC_MAX_TARGET_BILLS", "300")),
+            bill_batch_vote_sync_max_target_bills=int(_get_env("BILL_BATCH_VOTE_SYNC_MAX_TARGET_BILLS", "10000")),
 
             bill_batch_max_ai_per_run=int(_get_env("BILL_BATCH_MAX_AI_PER_RUN", "200")),
             bill_batch_ai_max_retry_count=int(_get_env("BILL_BATCH_AI_MAX_RETRY_COUNT", "3")),
